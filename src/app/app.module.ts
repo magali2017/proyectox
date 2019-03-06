621e1f91f1bfff7e37fb217f431555018a41478e
Magali Arana <aranamagali4@gmail.com>
2019-03-05 20:42:58 -0400

    creacion de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,19 @@ import { AdministracionComponent } from './components/administracion/administrac
 import { ReporteDiarioComponent } from './components/reporte-diario/reporte-diario.component';
 import { MiniReporteDiarioComponent } from './components/mini-reporte-diario/mini-reporte-diario.component';
 import { MiniReporteDiarioTotalizadoComponent } from './components/mini-reporte-diario-totalizado/mini-reporte-diario-totalizado.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const rutas:Routes=[
+  {path:'park_eventuales',component:EventualesComponent},
+  {path:'park_administracion',component:AdministracionComponent},
+  {path:'park_mensuales',component:MensualesComponent},
+  {path:'park_reporte_diario',component:ReporteDiarioComponent},
+  {path:'park_mini_reporte_diario',component:MiniReporteDiarioComponent},
+  {path:'park_mini_reporte_diario_totalizado',component:MiniReporteDiarioTotalizadoComponent},
+  //cuando ingresemos a la ruta principal
+  {path: '',component: HomeComponent,pathMatch:'full'},
+  //cualquier otra ruta
+  {path:'**', redirectTo:'./'}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +34,8 @@ import { MiniReporteDiarioTotalizadoComponent } from './components/mini-reporte-
     MiniReporteDiarioTotalizadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
